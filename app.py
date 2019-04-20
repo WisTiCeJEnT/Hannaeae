06d111da0c0fc491ce6b4c6b42846db6253fd63d
quazzer <natsamiss@gmail.com>
2019-04-20 16:55:00 +0700

    add comparision method :ok_hand:

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import line_api
 import facebook_api
 import json
 
+from scipy import spatial
+import deepcut
 app = Flask(__name__)
 CORS(app)
 
@@ -37,6 +39,27 @@ def facebookWebhook():
         return chal
     else:
         return "POST me some JSON"
+
+def message_tokenize(message):
+    return deepcut.tokenize(message)
+
+def message_comparison(msg1, msg2):
+    msg1_tokenize = message_tokenize(msg1)
+    msg2_tokenize = message_tokenize(msg2)
+    uniq_word = intersection(msg1_tokenize,msg2_tokenize)
+    d1 = {i:0 for i in uniq_word}
+    d2 = {i:0 for i in uniq_word}
+    for i in msg1_tokenize:
+        d1[i] += 1
+    for i in msg2_tokenize:
+        d2[i] += 1
+
+    v1 = [d1[i] for i in uniq_word]
+    v2 = [d2[i] for i in uniq_word]
+    return 1 - spatial.distance.cosine(v1,v2)
+
+def intersection(msg1, msg2):
+    return list(set(msg1+msg2))
 
 if __name__ == "__main__":
     app.run(debug = False,host="0.0.0.0", port=5000)
