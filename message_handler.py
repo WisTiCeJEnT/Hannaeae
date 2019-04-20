42561fa04dd8d4614e5c043a11e1c05a8c2d435e
quazzer <natsamiss@gmail.com>
2019-04-20 19:43:03 +0700

    compare request message with our joke in db

--- message_handler.py
+++ message_handler.py
@@ -1,16 +1,18 @@
 import deepcut
+import firebase_api
+import random
+from scipy import spatial
+import random
 def message_tokenize(message):
     return deepcut.tokenize(message)
 
 def message_comparison(msg1, msg2):
-    msg1_tokenize = message_tokenize(msg1)
-    msg2_tokenize = message_tokenize(msg2)
-    uniq_word = intersection(msg1_tokenize,msg2_tokenize)
+    uniq_word = intersection(msg1,msg2)
     d1 = {i:0 for i in uniq_word}
     d2 = {i:0 for i in uniq_word}
-    for i in msg1_tokenize:
+    for i in msg1:
         d1[i] += 1
-    for i in msg2_tokenize:
+    for i in msg2:
         d2[i] += 1
 
     v1 = [d1[i] for i in uniq_word]
@@ -19,3 +21,25 @@ def message_comparison(msg1, msg2):
 
 def intersection(msg1, msg2):
     return list(set(msg1+msg2))
+
+def get_most_similar_res_msg(req_msg):
+  response = dict(firebase_api.getAllMsg())
+  req_msg_tokenize = message_tokenize(req_msg)
+  answer = []
+  sum_score = 0
+  for i in response:
+      if 'req_msg_tokenize' not in response[i]:
+          continue
+      tokenize = response[i]['req_msg_tokenize']
+      score = message_comparison(req_msg_tokenize, tokenize)
+      if score > 0.5:
+          previous_score = sum_score
+          sum_score+=score
+          answer.append((previous_score, sum_score, response[i]['res_msg']))
+      ran_val = random.uniform(0, sum_score)
+  for i in answer:
+    if ran_val < i[1] and ran_val > i[0]:
+      return i[2]
+
+
+
